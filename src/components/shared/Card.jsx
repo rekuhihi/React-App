511b9cba72bc98e62723646e64b7f37daf6b5db0
Kovács Réka <kovacs.reka1020@gmail.com>
2022-04-20 17:39:13 +0200

    raect-app

---
+++ src/components/shared/Card.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types'
+
+function Card({children}) {
+  return (
+    <div className="card">
+    {children}
+    </div>
+  )
+}
+
+Card.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Card
